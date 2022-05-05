1a8693c592221d836b8bd0ce1789055fa28ed319
NuminousBlue <jdiehl2236@gmail.com>
2022-05-05 18:38:30 -0500

    basic paths defined

--- challenges/views.py
+++ challenges/views.py
@@ -1,3 +1,48 @@
+from django.http import HttpResponse
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+
+
+def index(request):
+   return HttpResponse("You've landed at the index page for app/challenges!")
+
+
+
+def january(request):
+   return HttpResponse("You've landed on January's challenge page!")
+
+def february(request):
+   return HttpResponse("You've landed on February's challenge page!")
+
+def march(request):
+   return HttpResponse("You've landed on March's challenge page!")
+
+def april(request):
+   return HttpResponse("You've landed on April's challenge page!")
+
+def may(request):
+   return HttpResponse("You've landed on May's challenge page!")
+
+def june(request):
+   return HttpResponse("You've landed on June's challenge page!")
+
+def july(request):
+   return HttpResponse("You've landed on July's challenge page!")
+
+def august(request):
+   return HttpResponse("You've landed on August's challenge page!")
+
+def september(request):
+   return HttpResponse("You've landed on September's challenge page!")
+
+def october(request):
+   return HttpResponse("You've landed on October's challenge page!")
+
+def november(request):
+   return HttpResponse("You've landed on November's challenge page!")
+
+def december(request):
+   return HttpResponse("You've landed on December's challenge page!")
+
